9a940dcda475e6fabfde3fc059877534dad2a378
Ana Stadelhofer <ana.stadelhofer@outlook.com>
2022-06-01 19:50:06 -0300

    Add Labels

--- Views/CadastrarSenha.cs
+++ Views/CadastrarSenha.cs
@@ -13,20 +13,47 @@ namespace Telas
         Form parent;
         Button btnVoltar;
         Button btnInsert;
+        Label lblNome;
+        Label lblCategoria;
+        Label lblUrl;
+        Label lblUser;
+        Label lblProcedimento;
+        Label lblTags;
 
         public CadastrarSenha(Form parent)
         {
             this.parent = parent;
 
-            btnVoltar = new Campos.ButtonField("Voltar", 50, 250, 100, 30);
+            this.lblNome = new Campos.LabelField("Nome:", 50, 50);
+
+            this.lblCategoria = new Campos.LabelField("Categoria:", 50, 150);
+
+            this.lblUrl = new Campos.LabelField("Url:", 50, 250);
+
+            this.lblUser = new Campos.LabelField("Usuário:", 50, 350);
+
+            this.lblProcedimento = new Campos.LabelField("Procedimento:", 50, 450);
+
+            this.lblTags = new Campos.LabelField("Tags:", 50, 550);
+
+            btnVoltar = new Campos.ButtonField("Voltar", 50, 750, 100, 30);
 			btnVoltar.Click += new EventHandler(this.btnVoltarClick);
 
-            btnInsert = new Campos.ButtonField("Confirmar", 150, 250, 100, 30);
+            btnInsert = new Campos.ButtonField("Confirmar", 150, 750, 100, 30);
 			btnInsert.Click += new EventHandler(this.btnInsertClick);
+
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblProcedimento);
+            this.Controls.Add(this.lblTags);
 
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.ClientSize = new System.Drawing.Size(300, 800);
             this.Text = "Cadastrar Senha";
         }
 
