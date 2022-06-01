d93b442d0df8e8852e1a20e52bb98a7d2c4794b2
Ana Stadelhofer <ana.stadelhofer@outlook.com>
2022-06-01 19:39:24 -0300

    Password Screen

--- Views/CadastrarSenha.cs
+++ Views/CadastrarSenha.cs
@@ -7,20 +7,16 @@ using System.Drawing;
 
 namespace Telas
 {
-    public class CadastrarTag : Form
+    public class CadastrarSenha : Form
     {
         private System.ComponentModel.IContainer components = null;
         Form parent;
-        TextBox txtDescricao;
-        Label lblDescricao;
         Button btnVoltar;
         Button btnInsert;
 
-        public CadastrarTag(Form parent)
+        public CadastrarSenha(Form parent)
         {
             this.parent = parent;
-            this.lblDescricao = new Campos.LabelField("Descrição:", 50, 80);
-            this.txtDescricao = new Campos.TextBoxField(50, 120, 200, 200);
 
             btnVoltar = new Campos.ButtonField("Voltar", 50, 250, 100, 30);
 			btnVoltar.Click += new EventHandler(this.btnVoltarClick);
@@ -28,32 +24,20 @@ namespace Telas
             btnInsert = new Campos.ButtonField("Confirmar", 150, 250, 100, 30);
 			btnInsert.Click += new EventHandler(this.btnInsertClick);
 
-            this.Controls.Add(this.lblDescricao);
-            this.Controls.Add(this.txtDescricao);
-            this.Controls.Add(this.btnVoltar);
-            this.Controls.Add(this.btnInsert);
-
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(300, 300);
-            this.Text = "Cadastrar Tag";
+            this.Text = "Cadastrar Senha";
         }
 
         public void btnVoltarClick(object sender, EventArgs e)
         {
             this.Close();
             this.parent.Show();
-        } 
+        }
 
         public void btnInsertClick(object sender, EventArgs e)
         {
-            this.Hide();
-            TagCtrl.InsertTag(this.txtDescricao.Text); 
-            TagView TagViews = new TagView(this);
-            TagViews.ShowDialog();
-            this.parent.Close();
-        }  
-        
+        } 
     }
-
 }
